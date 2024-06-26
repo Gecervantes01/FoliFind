1821831f660ed428e15d94fc76a8df2d9f6ba532
Gecervantes01 <cervantes.giovan@student.greenriver.edu>
2024-06-25 19:28:53 -0700

    Created a package (assets) and added a file called constants.dart, the purpose of this file is to store all constants used throughout the project. Could still use work looking into best practices for organizing the constants.

---
+++ folifind/lib/assets/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+///
+/// This file stores all constants used throughout the app. When
+/// adding a new constant ensure it:
+/// - has a descriptive name
+/// - follows camel case
+/// - located in the correct section
+/// If the constant doesn't have a corresponding section, please add
+/// one following the existing format.
+/// ------------------------------------------------------------------
+///
+/// USE THIS TO IMPORT TO ANOTHER FILE:
+/// - import 'package:folifind/assets/constants.dart' as Constants;
+/// 
+/// IN CODE UTILIZATION WOULD LOOK LIKE THIS:
+/// - width: Constants.nameOfConstant;
+///
+
+
+/////////////////////////////////////////////////////////////////////////////////
+/// plant_item.dart /////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+const double plantItemSize = 160;
+const Color plantItemBorderColor = Color.fromARGB(255, 247, 247, 247);
+const Color plantItemBackgroundColor = Color.fromARGB(255, 247, 247, 247);
