00ae28db2b5c2968e4e26a2ee296e50d5c755438
Gecervantes01 <cervantes.giovan@student.greenriver.edu>
2024-06-27 20:10:18 -0700

    The plants displayed in the Green house are now displayed dynamically and mimic how they'd appear with the implementation of an API.

--- folifind/lib/pages/greenhouse/green_house.dart
+++ folifind/lib/pages/greenhouse/green_house.dart
@@ -1,84 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:folifind/pages/greenhouse/plant_item.dart';
 import 'package:folifind/assets/constants.dart' as Constants;
+import 'package:folifind/tests/test_list.dart';
 
 /// Creates the Greenhouse page
+/// 
+/// @author: Giovan Cervantes
+/// @version: 1.0
 class GreenHouse extends StatelessWidget {
   const GreenHouse({super.key});
-  // ignore: constant_identifier_names
-  static const double PADDING_X = 30;
-  static const double PADDING_Y = 50;
 
   @override
   Widget build(BuildContext context) {
+
+    // Test data
+    List<String> randomPlants = TestList().randomPlants();
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('FOLIFIND'),
         backgroundColor: Constants.appBarBackgroundColor,
         ),
-      body: 
-        Column(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Align(
               alignment: Alignment.topLeft,
               child: Padding(
-                padding: EdgeInsets.only(
-                  left: 40, 
-                  top: 20
-                ),
+                padding: EdgeInsets.only(left: 40, top: 40),
                 child: Text(
+                  'My\nGreenhouse',
                   textScaler: TextScaler.linear(2.5),
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                   ),
-                  'My\nGreenhouse',
                 ),
                 
                 ),
             ),
-            Center(
-            heightFactor: 1.15,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 70, bottom: 10),
-                      child: SizedBox(child: Text('sort'),),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 70, bottom: 10),
-                      child: SizedBox(child: Icon(Icons.menu))
-                    )
-                  ],
+                Padding(
+                  padding: EdgeInsets.only(left: 70, bottom: 10),
+                  child: SizedBox(child: Text('sort'),),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(child: PlantItem(),),
-                    SizedBox(width: PADDING_X),
-                    Flexible(child: PlantItem(),),
-                  ],
-                ),
-                SizedBox(height: PADDING_Y),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Flexible(child: PlantItem(),),
-                    SizedBox(width: PADDING_X),
-                    Flexible(child: PlantItem(),),
-                  ],
-                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 70, bottom: 10),
+                  child: SizedBox(child: Icon(Icons.menu))
+                )
               ],
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1,
+                  ),
+                itemCount: randomPlants.length,
+                itemBuilder: (context, index) {
+                  String plantName = randomPlants[index];
+                  return PlantItem(plantName: plantName);
+                },
               ),
             ),
           ],
-        )
-      );
+      ),
+    );
   }
 }
--- folifind/lib/pages/greenhouse/plant_item.dart
+++ folifind/lib/pages/greenhouse/plant_item.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:folifind/assets/constants.dart' as Constants;
 
-/* 
-  This class is going to represent a plant item, which is visible within 
-  the "Greenhouse". 
-  Each plant item is going to include:
-  - Plant Name
-  - Image of the plant
-  Relevant arguement data will only be retreived when user chooses to add
-  a plant to their greenhouse.
+///
+/// This class is going to represent a plant item, which is visible within 
+/// the "Greenhouse". 
+/// Each plant item is going to include:
+/// - Plant Name
+/// - Image of the plant
+/// Relevant arguement data will only be retreived when user chooses to add
+/// a plant to their greenhouse.
+/// 
+/// @Author: Giovan Cervantes
+/// @Version: 1.0
 
-  @Author: Giovan Cervantes
-  @Version: 1.0
-*/
-class PlantItem extends StatelessWidget{
-  const PlantItem({super.key});
+class PlantItem extends StatelessWidget {
+  final String plantName;
+
+  /// Constructor which currently requires name of the plant
+  const PlantItem({required this.plantName});
 
   @override
   Widget build(BuildContext context) {
@@ -25,14 +28,14 @@ class PlantItem extends StatelessWidget{
           height: Constants.plantItemSize,
           decoration: BoxDecoration(
             border: Border.all(
-              width: 1.0,
+              width: 1,
               color: Constants.plantItemBorderColor,
             ),
             borderRadius: BorderRadius.all(Radius.circular(30.0)),
             color: Constants.plantItemBackgroundColor,
             ),
           ),
-          Text('Plant Name'),
+          Text(plantName),
       ]
     );
   }
